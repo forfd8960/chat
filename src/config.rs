7829c35d78e72240da20c2f642889f14027e3176
forfd8960 <forfd8960@gmail.com>
2024-07-03 09:56:44 +0800

    feature: [config.rs]read config from app.yml

---
+++ src/config.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+
+use std::{env, fs::File};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AppConfig {
+    pub server: ServerConfig,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ServerConfig {
+    pub host: u16,
+}
+
+#[allow(dead_code)]
+impl AppConfig {
+    pub fn load() -> anyhow::Result<Self> {
+        let cfg = match (
+            File::open("app.yml"),
+            File::open("/etc/chat/app.yml"),
+            env::var("CHAT_CONFIG"),
+        ) {
+            (Ok(f), _, _) => serde_yml::from_reader(f),
+            (_, Ok(f), _) => serde_yml::from_reader(f),
+            (_, _, Ok(f)) => serde_yml::from_reader(File::open(f)?),
+            _ => anyhow::bail!("Could not find app.yml"),
+        };
+
+        anyhow::Ok(cfg?)
+    }
+}
