6a8237c804de01366b4e20171e6bda35478ee834
forfd8960 <forfd8960@gmail.com>
2024-07-05 23:09:58 +0800

    feature: Notify Server - SSE Handler

--- notify_server/src/main.rs
+++ notify_server/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+use tokio::net::TcpListener;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
+
+use notify_server::get_router;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let addr = "0.0.0.0:8989";
+
+    let listener = TcpListener::bind(&addr).await?;
+    info!("Listening on: {}", addr);
+
+    let app = get_router();
+    axum::serve(listener, app.into_make_service()).await?;
+
+    anyhow::Ok(())
 }
